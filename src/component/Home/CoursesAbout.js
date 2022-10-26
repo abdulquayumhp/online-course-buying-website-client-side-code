b1424e45b08d63d290fe94e55e503b2f64ae6545
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-10-27 02:06:40 +0600

    details seciton some custiomize

--- src/component/Home/CoursesAbout.js
+++ src/component/Home/CoursesAbout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegEye } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const CoursesAbout = ({ curse }) => {
@@ -35,7 +36,9 @@ const CoursesAbout = ({ curse }) => {
 								<Link className="bg-slate-100 py-2 px-4" to={`/courses/${id}`}>
 									Know More
 								</Link>
-								<span>{totalView}</span>
+								<span className="flex items-center">
+									<FaRegEye className="mr-2" /> {totalView}
+								</span>
 							</div>
 						</div>
 					</article>
